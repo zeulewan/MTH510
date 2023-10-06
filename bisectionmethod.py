3dcfe289d2a75b11d1bd818d8b73a0828f286a84
Zeulewan Mordasiewicz <zeul@mordasiewicz.com>
2023-10-06 12:05:22 -0400

    init

---
+++ bisectionmethod.py
@@ -0,0 +1,27 @@
+low = -1
+high = 3
+
+def f(x):
+    return (x*x*x*x*x*x*x - 2)
+i = 0
+while (f(low) * f(high) < 0):
+    c = (low + high)/2
+
+    if (abs(c - high) < 0.0001) or (abs(c - low) < 0.0001):
+        print("root is x =", c)
+        i = i + 1
+        break
+    else: 
+        if(f(low) * f(c) < 0):
+            i = i + 1
+            high = c
+        else: 
+            if(f(high) * f(c) < 0):
+                low = c
+                i = i + 1
+
+    print (c)
+print(i)
+
+if c is None:
+    print("uneven # of roots in range")
